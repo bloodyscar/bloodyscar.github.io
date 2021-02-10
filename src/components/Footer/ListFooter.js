9943187f6444c834f5731c67062e778f3f22c350
bloodyscar <enggataukk@gmail.com>
2021-02-10 08:58:03 +0700

    ready deploy to netlify

--- src/components/Footer/ListFooter.js
+++ src/components/Footer/ListFooter.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './ListFooter.css'
+
 
 function ListFooter(props) {
+    const[isOpen, setIsOpen] = useState(false)
+    console.log(window)
+
+    const handleFooter = () => {
+        setIsOpen(!isOpen)
+    }
+    console.log(isOpen)
     return (
-        <div class="md:w-1/4 relative mb-2 ml-2 mt-3">
-            <h5 class="text-lg font-semibold mb-2 relative text-white">{props.header}</h5>
-            <ul class="h-0 invisible md:h-auto md:visible overflow-hidden">
+        <div className="md:w-1/4 relative mb-2 ml-2 mt-3">
+            <div className="flex items-center justify-between" >
+                <h5 className="text-lg font-semibold mb-2 relative text-white">{props.header}</h5>
+
+                <svg onClick={handleFooter} className={`${isOpen ? 'transform rotate-180' : ''} md:hidden cursor-pointer`} width="20" height="9" viewBox="0 0 20 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 1L9.75 7.5L18.5 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </div>
+
+            
+            <ul className={`${isOpen ? 'visible footer-menu active' : 'footer-menu'} md:visible  `}>
                 <li>
-                    <a href="#" class="hover:underline py-1 block text-white">{props.menu1}</a>
+                    <a href="#" className="hover:underline py-1 block text-white">{props.menu1}</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline py-1 block text-white">
+                    <a href="#" className="hover:underline py-1 block text-white">
                         {props.menu2}
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline py-1 block text-white">{props.menu3}</a>
+                    <a href="#" className="hover:underline py-1 block text-white">{props.menu3}</a>
                 </li>
             </ul>
         </div>
