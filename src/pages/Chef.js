bfe62970314d46e09ec0ea643d6e556e37efc8e8
bloodyscar <enggataukk@gmail.com>
2021-02-10 09:15:36 +0700

    ready deploy to netlify

--- src/pages/Chef.js
+++ src/pages/Chef.js
@@ -5,8 +5,8 @@ function Chef() {
     return (
         <>
             <div className="flex justify-center items-center h-screen leading-tight">
-                <h1 className="text-3xl md:text-6xl text-red-500 animate-bounce text-center ">Sorry, <br/> 
-                    <Typical loop={Infinity}  steps={['404 NOT FOUND', 1000, '', 1000]} />
+                <h1 className="text-3xl md:text-6xl text-red-500 animate-bounce text-center ">Sorry,<br />
+                    <Typical loop={Infinity} steps={['404 NOT FOUND', 1000, '', 1000]} />
                 </h1>
             </div>
         </>
